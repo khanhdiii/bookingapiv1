ac462a961b8ab10d18d5748d7ff5c9c768b61a75
khanhdiii <khanh12369874@gmail.com>
2023-04-26 12:39:29 +0700

    fix take token

--- utils/veryfyToken.js
+++ utils/veryfyToken.js
@@ -3,6 +3,7 @@ import { createError } from "../utils/error.js";
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies?.access_token
+
     if (!token) {
         return next(createError(401, "You are not authenticated! "))
     }
@@ -28,7 +29,7 @@ export const verifyUser = (req, res, next) => {
 }
 
 export const verifyAdmin = (req, res, next) => {
-    verifyToken(req, res, next, () => {
+    verifyToken(req, res, () => {
         if (req.user.isAdmin) {
             next()
         }
