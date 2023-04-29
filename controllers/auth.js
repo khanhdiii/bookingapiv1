ece2ce45c07f0046b7bbcb95aa4b359ed994c369
khanhdiii <khanh12369874@gmail.com>
2023-04-29 18:36:43 +0700

    fix bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,7 +39,7 @@ export const login = async (req, res, next) => {
 
         // const userDetails = Array.from(orderDetails) // convert to array
         res.cookie("access_token", token, {
-            httpOnly: true,
+            httpOnly: true, sameSite: "none", secure: true,
         }).status(200).json({ details: { ...otherDetails }, isAdmin })
     } catch (err) {
         next(err)
