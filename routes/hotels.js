160955815a81d08230df808d4b789704a2df94e1
khanhdiii <khanh12369874@gmail.com>
2023-05-08 04:36:44 +0700

    delete verify

--- routes/hotels.js
+++ routes/hotels.js
@@ -14,9 +14,9 @@ import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
 
-router.post("/", verifyAdmin, createHotel);
-router.put("/:id", verifyAdmin, updateHotel);
-router.delete("/:id", verifyAdmin, deleteHotel);
+router.post("/", createHotel);
+router.put("/:id", updateHotel);
+router.delete("/:id", deleteHotel);
 router.get("/find/:id", getHotel);
 router.get("/", getHotels);
 router.get("/countByCity", countByCity);
